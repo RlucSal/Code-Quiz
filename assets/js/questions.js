417b80c87501af196e6c3aec17ce766d486f1e28
Rosa Lucena <145674108+RlucSal@users.noreply.github.com>
2023-11-22 11:06:18 -0500

    Update questions.js
    Added array of questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,151 +1,151 @@
-let questions =[
+var questionsArray =[
 
 {
-    Question: 'JavaScript is the same as Java.',
-    Choises : ['False', 'True'],
-    CorrectAnswer: 'False',
-},
-
-{
-    Question: 'Is JavaScript case-sensitive?',
-    Choises : ['Yes', 'No'],
-    CorrectAnswer: 'Yes',
+    question: 'JavaScript is the same as Java.',
+    choices : ['False', 'True'],
+    correctAnswer: 0,
 },
 
 {
-    Question: 'Inside which HTML element do we put the JavaScript?',
-    Choises : ['<scripting', '<js>', '<javascript>', '<script>'],
-    CorrectAnswer: '<script>',
+    question: 'Is JavaScript case-sensitive?',
+    choices : ['Yes', 'No'],
+    correctAnswer: 0,
 },
 
 {
-    Question: 'What is the correct JavaScript syntax to change the content of the HTML element <p id= "demo ">This is a demostration</p>',
-    Choises : ['document.getElement("p").innerHTML="Hello word"', 'document.getElementById("demo").innerHTML="Hello World"', 'document.getElementByName("p").innerHTML= "Hello World"', '#demo.innerHTML="Hello World"'],
-    CorrectAnswer: 'document.getElementById ("demo").innerHTML= "Hello World"',
+    question: 'Inside which HTML element do we put the JavaScript?',
+    choices : ['<scripting', '<js>', '<javascript>', '<script>'],
+    correctAnswer: 3,
 },
 
 {
-    Question: 'Where is the correct place to insert a JavaScript?',
-    Choises : ['The <head> section', 'The <body> section', 'Both the <head> section and the <body>section are correct', 'No answer above are correct'],
-    CorrectAnswer:  'Both the <head> section and the <body>section are correct',
+    question: 'What is the correct JavaScript syntax to change the content of the HTML element <p id= "demo ">This is a demostration</p>',
+    choices : ['document.getElement("p").innerHTML="Hello word"', 'document.getElementById("demo").innerHTML="Hello World"', 'document.getElementByName("p").innerHTML= "Hello World"', '#demo.innerHTML="Hello World"'],
+    correctAnswer: 1,
 },
 
 {
-    Question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-    Choises : ['<script src="xxx.js">', '<script name=xxx.js>','<script href=xxx.js>', '<script=xxx.js>'],
-    CorrectAnswer: '<script src="xxx.js">',
+    question: 'Where is the correct place to insert a JavaScript?',
+    choices : ['The <head> section', 'The <body> section', 'Both the <head> section and the <body>section are correct', 'No answer above are correct'],
+    correctAnswer:  2,
 },
 
 {
-    Question: 'The external JavaScript file must contain the <script> tag.',
-    Choises : ['True', 'False'],
-    CorrectAnswer: 'False',
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    choices : ['<script src="xxx.js">', '<script name=xxx.js>','<script href=xxx.js>', '<script=xxx.js>'],
+    correctAnswer: 0,
 },
 
 {
-    Question: 'How do you write "Hello World" in an alert box?',
-    Choises : ['alert("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")', 'msg("Hello World")'],
-    CorrectAnswer: 'alert("Hello World")',
+    question: 'The external JavaScript file must contain the <script> tag.',
+    choices : ['True', 'False'],
+    correctAnswer: 0,
 },
 
 {
-    Question: 'How do you create a function in JavaScript?',
-    Choises : ['function = myFunction()', 'function:myFunction()', 'function myFunction()'], 
-    CorrectAnswer: 'function myFunction()',
+    question: 'How do you write "Hello World" in an alert box?',
+    choices : ['alert("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")', 'msg("Hello World")'],
+    correctAnswer: 0,
 },
 
 {
-    Question: 'How do you call a function named "myFunction"?',
-    Choises : ['myFunction()', 'call myFunction()', 'call function myFunction()'], 
-    CorrectAnswer: 'myFunction()',
+    question: 'How do you create a function in JavaScript?',
+    choices : ['function = myFunction()', 'function:myFunction()', 'function myFunction()'], 
+    correctAnswer: 2,
 },
 
 {
-    Question: 'How to write an IF statement in JavaScript?',
-    Choises : ['if i = 5 then', 'if i = 5', 'if (i === 5)', 'if i == 5 then'],
-    CorrectAnswer:  'if (i === 5)',
+    question: 'How do you call a function named "myFunction"?',
+    choices : ['myFunction()', 'call myFunction()', 'call function myFunction()'], 
+    correctAnswer: 0,
 },
 
 {
-    Question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
-    Choises : ['if (i != 5)', 'if i <> 5', 'if i != 5 then', 'if (i <> 5)'],
-    CorrectAnswer: 'if (i != 5)',
+    question: 'How to write an IF statement in JavaScript?',
+    choices : ['if i = 5 then', 'if i = 5', 'if (i === 5)', 'if i == 5 then'],
+    correctAnswer:  2,
 },
 
 {
-    Question: 'How does a WHILE loop start?',
-    Choises : ['while (i <= 10)', 'while (i <= 10; i++', 'while i = 1 to 10'], 
-    CorrectAnswer: 'while (i <= 10)',
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    choices : ['if (i != 5)', 'if i <> 5', 'if i != 5 then', 'if (i <> 5)'],
+    correctAnswer: 0,
 },
 
 {
-    Question: 'How does a FOR loop start?',
-    Choises : ['for (i <= 5; i++)', 'for i = 1 to 10', 'for ( i = 0; i <= 5)', 'for (i = 0; i <= 5; i++)'],
-    CorrectAnswer: 'for (i = 0; i <= 5; i++)'
+    question: 'How does a WHILE loop start?',
+    choices : ['while (i <= 10)', 'while (i <= 10; i++', 'while i = 1 to 10'], 
+    correctAnswer: 0,
 },
 
 {
-    Question: 'How can you add a comment in a JavaScript?',
-    Choises : ['<!--This is a comment-->', '"This is a comment"', '//This is a comment'], 
-    CorrectAnswer: '//This is a comment',
+    question: 'How does a FOR loop start?',
+    choices : ['for (i <= 5; i++)', 'for i = 1 to 10', 'for ( i = 0; i <= 5)', 'for (i = 0; i <= 5; i++)'],
+    correctAnswer: 3,
 },
 
 {
-    Question: 'What is the correct way to write a JavaScript array?',
-    Choises : ['var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = ["red", "green", "blue"]', 'var colors =  "red", "green", "blue"', 'var colors = (1: "red",2: "green", 3: "blue")'],
-    CorrectAnswer:  'var colors = ["red", "green", "blue"]',
+    question: 'How can you add a comment in a JavaScript?',
+    choices : ['<!--This is a comment-->', '"This is a comment"', '//This is a comment'], 
+    correctAnswer: 2,
 },
 
 {
-    Question: 'How do you round the number 7.25, to the nearest integer?',
-    Choises : ['round(7.25)', 'rnd(7.25)', 'Math.rnd(7.25)', 'Math.round(7.25)'],
-    CorrectAnswer: 'Math.round(7.25)',
+    question: 'What is the correct way to write a JavaScript array?',
+    choices : ['var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = ["red", "green", "blue"]', 'var colors =  "red", "green", "blue"', 'var colors = (1: "red",2: "green", 3: "blue")'],
+    correctAnswer:  1,
 },
 
 {
-    Question: 'Which event occurs when the user clicks on an HTML element?',
-    Choises : ['onmouseclick', 'onclick', 'dblclick', 'onmouseover',],
-    CorrectAnswer: 'onclick',
+    question: 'How do you round the number 7.25, to the nearest integer?',
+    choices : ['round(7.25)', 'rnd(7.25)', 'Math.rnd(7.25)', 'Math.round(7.25)'],
+    correctAnswer: 3,
 },
 
 {
-    Question: 'How do you declare a JavaScript variable?',
-    Choises : ['vcarName', 'variable carName', 'var carName'],
-    CorrectAnswer: 'var carName',
+    question: 'Which event occurs when the user clicks on an HTML element?',
+    choices : ['onmouseclick', 'onclick', 'dblclick', 'onmouseover',],
+    correctAnswer: 1,
 },
 
 {
-    Question: 'Which operator is used to assign a value to a variable?',
-    Choises : ['x','*', '=', '-'],
-    CorrectAnswer: '=',
+    question: 'How do you declare a JavaScript variable?',
+    choices : ['vcarName', 'variable carName', 'var carName'],
+    correctAnswer: 2,
 },
 
 {
-    Question: 'What will the following code return: Boolean(10 > 9)',
-    Choises : ['NaN', 'False', 'True'],
-    CorrectAnswer: 'True',
-},
-{
-    Question: ' How do you read the first character in a string ?',
-    Choises : ['stringName.charAt(0)', 'stringName.substring(1)','stringName.charAt(1)','stringName.charAt(2)'],
-    CorrectAnswer: 'stringName.charAt(0)',
-},
-{
-    Question: 'What is DOM ?',
-    Choises : ['Dynamic Object Model', 'Document Object Model','Distributed Object Model', 'None of these'],
-    CorrectAnswer: 'Document Object Model',
+    question: 'Which operator is used to assign a value to a variable?',
+    choices : ['x','*', '=', '-'],
+    correctAnswer:2,
 },
 
 {
-    Question: 'Suppose that ele is an element node. Select all the expressions below which can be used to select the parent node of ele.',
-    Choises : ['ele.parentElement', 'ele.parentContainer', 'ele.parentNode', 'ele.getParent()'],
-    CorrectAnswer: 'ele.parentElement',
+    question: 'What will the following code return: Boolean(10 > 9)',
+    choices : ['NaN', 'False', 'True'],
+    correctAnswer: 2,
+},
+{
+    question: ' How do you read the first character in a string ?',
+    choices : ['stringName.charAt(0)', 'stringName.substring(1)','stringName.charAt(1)','stringName.charAt(2)'],
+    correctAnswer: 0,
+},
+{
+    question: 'What is DOM ?',
+    choices : ['Dynamic Object Model', 'Document Object Model','Distributed Object Model', 'None of these'],
+    correctAnswer: 1,
 },
 
 {
-    Question: 'How to select the first element in an HTML document matching the CSS selector .main .title .t1?',
-    Choises : ['document.querySelector(".main .title .t1")', 'document.querySelectorAll(".main .title .tl")', 'document.body.matches(".main .title .tl")'],
-    CorrectAnswer: 'document.querySelector(".main .title .t1")'
+    question: 'Suppose that ele is an element node. Select all the expressions below which can be used to select the parent node of ele.',
+    choices : ['ele.parentElement', 'ele.parentContainer', 'ele.parentNode', 'ele.getParent()'],
+    correctAnswer: 0,
+},
+
+{
+    question: 'How to select the first element in an HTML document matching the CSS selector .main .title .t1?',
+    choices : ['document.querySelector(".main .title .t1")', 'document.querySelectorAll(".main .title .tl")', 'document.body.matches(".main .title .tl")'],
+    correctAnswer: 0,
 },
 
 
